ab634da9559247a6cc5532e00c2ea0f38cf2feb1
FrankLuqi <1014206599@qq.com>
2018-09-14 15:55:50 +0800

    add header.vue

--- src/main.js
+++ src/main.js
@@ -3,8 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+//  引入通用布局
+import './assets/styles/reset.css'
+//  引入1像素边框解决方案
+import './assets/styles/border.css'
+//  解决移动端单击300毫秒延迟问题 在项目所在目录中执行目录cnpm install fastclick --save
+import fastClick from 'fastclick'
+//  引入iconfont
+import './assets/styles/iconfont.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="header">
+      <div class="header-left"><div class="iconfont icon-back">&#xe628;</div></div>
+      <div class="header-input"><span class="iconfont">&#xe648;</span>请输入游玩景点</div>
+      <div class="header-right">城市<span class="iconfont icon-attr">&#xe662;</span></div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+<style lang="stylus" scoped>
+/*在css中使用别名时前面要加~*/
+@import '~styles/varibles.styl'
+  .header
+    /* 1rem 等于 50px */
+    height .86rem
+    background-color $bg_color
+    color #fff
+    display flex
+    line-height .86rem
+    .header-left
+      width .64rem
+      .icon-back
+        text-align center
+        font-size .4rem
+    .header-input
+      flex 1
+      height .64rem
+      line-height .64rem
+      margin-top .12rem
+      margin-left .2rem
+      border-radius .1rem
+      background-color #fff
+      color #ccc
+      padding-left .2rem
+    .header-right
+      width 1.24rem
+      text-align center
+      .icon-attr
+        font-size .24rem
+</style>
